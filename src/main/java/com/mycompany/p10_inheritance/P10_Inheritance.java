b36faa1c472f7cc176285bbc070fc01670c60bb1
lenovo <ramadlan2019@gmail.com>
2024-06-30 20:04:13 +0700

    ca

---
+++ src/main/java/com/mycompany/p10_inheritance/P10_Inheritance.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.p10_inheritance;
+
+/**
+ *
+ * @author lenovo
+ */
+public class P10_Inheritance {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
